ab07ed2b956d43ab81d0130d4d58be592b8e0f1e
Robert Gleason <l2ob122@gmail.com>
2024-12-19 16:46:08 -0800

    created components for the job card, layout, section heading and tag

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,17 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+
+    <div class="self-start text-sm">Laracasts</div>
+    <div class="py-8 font-bold">
+        <h3>Developer</h3>
+        <p>Full Time - From $50,000</p>
+    </div>
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <x-tag>tag</x-tag>
+            <x-tag>tag</x-tag>
+            <x-tag>tag</x-tag>
+
+        </div>
+        <img src="https://picsum.photos/seed/1734654183567/42/42" alt="" srcset="" class="rounded">
+    </div>
+</div>
